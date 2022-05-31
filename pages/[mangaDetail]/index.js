c98b9917dd841d87cbdd827756dade419be6b3a0
Gri-ffin <gameshunter359@gmail.com>
2022-05-31 19:06:09 +0100

    manga detail

--- pages/[mangaDetail]/index.js
+++ pages/[mangaDetail]/index.js
@@ -1,10 +1,81 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import MFA from 'mangadex-full-api';
 
 const MangaDetail = ({ mangaDetail }) => {
-  console.log(mangaDetail);
-  return <Box>Hello</Box>;
+  console.log(mangaDetail.chapters);
+
+  return (
+    <Box>
+      <Center as='h2' fontSize={40}>
+        {mangaDetail.title}
+      </Center>
+      <Flex
+        justifyItems='center'
+        alignItems='center'
+        flexDir={{ sm: 'column', md: 'row' }}
+        flex={1}
+      >
+        <Box marginLeft={{ md: 20 }} lineHeight={7}>
+          <Image
+            w={96}
+            h='xl'
+            src={mangaDetail.coverImage}
+            alt={mangaDetail.title}
+          />
+          <Text fontWeight='bold' textDecor='underline' textUnderlineOffset={3}>
+            Status:{' '}
+            <Text as='span' fontWeight='medium'>
+              {mangaDetail.status}
+            </Text>
+          </Text>
+          <Text fontWeight='bold' textDecor='underline' textUnderlineOffset={3}>
+            Content Rating:{' '}
+            <Text as='span' fontWeight='medium'>
+              {mangaDetail.contentRating}
+            </Text>
+          </Text>
+        </Box>
+        <Box mx={9} w={{ md: '40%' }} alignSelf='start' ml={{ sm: 14 }}>
+          <Text
+            as='h3'
+            fontSize={32}
+            textDecor='underline'
+            textUnderlineOffset={3}
+          >
+            Description
+          </Text>
+          <Text
+            fontSize={{ sm: 'md', md: 'xl' }}
+            lineHeight={{ base: 7, md: 8 }}
+          >
+            {mangaDetail.description}
+          </Text>
+          <Text
+            as='h3'
+            fontSize={24}
+            my={5}
+            textDecor='underline'
+            textUnderlineOffset={3}
+          >
+            Chapters
+          </Text>
+          <Box pt={1} pl={3} overflowY='scroll' h={96} w='full' bg='gray.700'>
+            {mangaDetail.chapters.map(chapter => {
+              return (
+                <Text key={chapter.id} my={3} fontWeight='bold'>
+                  Volume {chapter.volume}:{' '}
+                  <Text as='span' fontWeight='medium'>
+                    {chapter.title}
+                  </Text>
+                </Text>
+              );
+            })}
+          </Box>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default MangaDetail;
@@ -34,6 +105,9 @@ export async function getStaticProps(context) {
       chapter: chapter.chapter,
       title: chapter.title
     };
+  });
+  transformedChapters.sort((a, b) => {
+    return +a.chapter - +b.chapter;
   });
   let returnValue = {
     id: result.id,
