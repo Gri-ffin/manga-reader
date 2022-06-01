3a2ba0ab9d1cce0eedf69025faaa7048117e8694
Gri-ffin <gameshunter359@gmail.com>
2022-06-01 18:06:34 +0100

    external site svg

---
+++ utils/ExternalSiteSVG.jsx
@@ -0,0 +1,28 @@
+export const ExternalSiteSVG = () => {
+  return (
+    <svg
+      style={{
+        display: 'inline-block',
+        verticalAlign: 'middle'
+      }}
+      data-v-20f285ec=''
+      data-v-0c0c5a2c=''
+      width='20'
+      height='20'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className='mr-1 small text-icon-black dark:text-icon-white text-false icon'
+      data-v-85283b92=''
+    >
+      <path
+        data-v-20f285ec=''
+        d='M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6m4-3h6v6m-11 5L21 3'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      ></path>
+    </svg>
+  );
+};
