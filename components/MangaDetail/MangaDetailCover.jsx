75c81dcc41a94014bc2e60451a574487a1121a1f
Gri-ffin <gameshunter359@gmail.com>
2022-06-01 18:41:51 +0100

    fix style

--- components/MangaDetail/MangaDetailCover.jsx
+++ components/MangaDetail/MangaDetailCover.jsx
@@ -23,18 +23,13 @@ const MangaDetailCover = ({ mangaDetail }) => {
           {mangaDetail.status}
         </Text>
       </Text>
-      <Text
-        fontWeight='bold'
-        textDecor='underline'
-        textUnderlineOffset={3}
-        mb={2}
-      >
+      <Text fontWeight='bold' textDecor='underline' textUnderlineOffset={3}>
         Content Rating:{' '}
         <Text as='span' fontWeight='medium'>
           {mangaDetail.contentRating}
         </Text>
       </Text>
-      <Text display={{ base: 'none', md: 'block' }}>
+      <Text display={{ base: 'none', md: 'block' }} mb={2}>
         Tags: {tags.map(tag => tag.localizedName.en).join(', ')}
       </Text>
       <Link href={mangaDetail.mal} isExternal>
