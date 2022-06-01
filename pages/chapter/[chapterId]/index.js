c23c699985cbe8292cf671a76ac6eca742de35c3
Gri-ffin <gameshunter359@gmail.com>
2022-06-01 18:07:48 +0100

    chapter page | closes #7

---
+++ pages/chapter/[chapterId]/index.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+const ChapterPage = () => {
+  const [pages, setPages] = useState([]);
+  const router = useRouter();
+  let chapterId = router.query.chapterId;
+
+  useEffect(() => {
+    async function fetchPages() {
+      const { data } = await axios.post('/api/fetch-pages', {
+        chapterId: chapterId
+      });
+      setPages(data.pages);
+    }
+    fetchPages();
+  }, [chapterId]);
+
+  console.log(pages);
+
+  return (
+    <Flex alignItems='center' justifyContent='center' flexDir='column'>
+      {pages?.map((page, i) => (
+        <Image
+          src={page}
+          height={500}
+          width={500}
+          key={i}
+          alt={`page ${i + 1}`}
+        />
+      ))}
+    </Flex>
+  );
+};
+
+export default ChapterPage;
