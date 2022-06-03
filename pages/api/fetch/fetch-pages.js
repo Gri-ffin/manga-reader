078cfc66d297154e082abca34f5e27148b8d6bc0
Gri-ffin <gameshunter359@gmail.com>
2022-06-03 18:25:59 +0100

    remove unused response

--- pages/api/fetch/fetch-pages.js
+++ pages/api/fetch/fetch-pages.js
@@ -1,33 +1,34 @@
 import MFA from 'mangadex-full-api';
 
 export default async function handler(req, res) {
-  const chapterId = req.body.chapterId;
-  try {
-    await MFA.login(process.env.Username, process.env.Password);
-    const result = await MFA.Chapter.get(chapterId);
-    const resultPages = await result.getReadablePages();
-    const manga = await result.manga.resolve();
-    const chapters = await manga.getFeed(
-      {
-        translatedLanguage: ['en'],
-        order: { chapter: 'asc' },
-        limit: Infinity
-      },
-      false
-    );
-    const filteredChapters = chapters.filter(chapter => {
-      return chapter.isExternal === false;
-    });
-    const transformedChapters = filteredChapters.map(chapter => ({
-      id: chapter.id,
-      chapter: chapter.chapter
-    }));
-    return res.json({
-      pages: resultPages,
-      chapters: transformedChapters,
-      thisChapter: result.chapter
-    });
-  } catch (e) {
-    res.json({ error: e.message });
+  if (req.method === 'POST') {
+    const chapterId = req.body.chapterId;
+    try {
+      await MFA.login(process.env.Username, process.env.Password);
+      const result = await MFA.Chapter.get(chapterId);
+      const resultPages = await result.getReadablePages();
+      const manga = await result.manga.resolve();
+      const chapters = await manga.getFeed(
+        {
+          translatedLanguage: ['en'],
+          order: { chapter: 'asc' },
+          limit: Infinity
+        },
+        false
+      );
+      const filteredChapters = chapters.filter(chapter => {
+        return chapter.isExternal === false;
+      });
+      const transformedChapters = filteredChapters.map(chapter => ({
+        id: chapter.id,
+        chapter: chapter.chapter
+      }));
+      return res.json({
+        pages: resultPages,
+        chapters: transformedChapters
+      });
+    } catch (e) {
+      res.json({ error: e.message });
+    }
   }
 }
