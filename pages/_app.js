bdb9a7577014eca657480fbab32905721aa5db15
Gri-ffin <gameshunter359@gmail.com>
2022-06-05 16:50:45 +0100

    loading state when switching pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <NextNProgress />
       <Component {...pageProps} />
     </ChakraProvider>
   );
