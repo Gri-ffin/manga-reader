4c5f7354ea202b6e13ac0f7be5fdfd16f2a8600b
Gri-ffin <gameshunter359@gmail.com>
2022-06-05 19:33:45 +0100

    credit mangadex and copyright

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { Center, ChakraProvider } from '@chakra-ui/react';
 import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
+  let year = new Date().getFullYear();
   return (
     <ChakraProvider>
       <NextNProgress />
       <Component {...pageProps} />
+      <Center as='footer' mt={6} mb={2}>
+        Powered by Mangadex - Yassine Tadlaoui &copy; {year}
+      </Center>
     </ChakraProvider>
   );
 }
