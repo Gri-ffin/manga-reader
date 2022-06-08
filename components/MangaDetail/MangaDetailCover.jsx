e0bcaccea6f666549e64c21dcaf91e7c4876f8c2
Gri-ffin <gameshunter359@gmail.com>
2022-06-08 17:20:50 +0100

    refractor code | closes #27 #26

--- components/MangaDetail/MangaDetailCover.jsx
+++ components/MangaDetail/MangaDetailCover.jsx
@@ -1,8 +1,10 @@
-import { Box, Flex, Link, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { MALSvg } from '../../utils/myanimelist';
 import Image from 'next/image';
 import { AniList } from '../../utils/anilist';
 import { MangaUpdate } from '../../utils/mangaupdate';
+import MangaCoverDetail from './MangaCoverDetail';
+import Track from './Track';
 
 const MangaDetailCover = ({ mangaDetail }) => {
   const tags = JSON.parse(mangaDetail.tags);
@@ -17,33 +19,15 @@ const MangaDetailCover = ({ mangaDetail }) => {
         placeholder='blur'
         blurDataURL={mangaDetail.coverImage}
       />
-      <Text
-        fontWeight='bold'
-        textDecor='underline'
-        textUnderlineOffset={3}
-        textAlign='start'
-        ml={{ base: 12, md: 0 }}
-      >
-        Status:{' '}
-        <Text as='span' fontWeight='medium'>
-          {mangaDetail.status}
-        </Text>
-      </Text>
-      <Text
-        fontWeight='bold'
-        textDecor='underline'
-        textUnderlineOffset={3}
-        textAlign='start'
-        ml={{ base: 12, md: 0 }}
-      >
-        Content Rating:{' '}
-        <Text as='span' fontWeight='medium'>
-          {mangaDetail.contentRating}
-        </Text>
-      </Text>
-      <Text display={{ base: 'none', md: 'block' }} lineHeight={7} w={96}>
-        Tags: {tags.map(tag => tag.localizedName.en).join(', ')}
-      </Text>
+      <MangaCoverDetail title='Status' info={mangaDetail.status} />
+      <MangaCoverDetail
+        title='Content Rating'
+        info={mangaDetail.contentRating}
+      />
+      <MangaCoverDetail
+        title='Tags'
+        info={tags.map(tag => tag.localizedName.en).join(', ')}
+      />
       <Text
         fontWeight='bold'
         fontSize='large'
@@ -55,21 +39,15 @@ const MangaDetailCover = ({ mangaDetail }) => {
         Track
       </Text>
       <Flex justifyContent='space-around' mt={2}>
-        {mangaDetail.mal && (
-          <Link href={mangaDetail.mal} isExternal>
-            <MALSvg />
-          </Link>
-        )}
-        {mangaDetail.aniList && (
-          <Link href={mangaDetail.aniList} isExternal>
-            <AniList />
-          </Link>
-        )}
-        {mangaDetail.mangaUpdates && (
-          <Link href={mangaDetail.mangaUpdates} isExternal>
-            <MangaUpdate />
-          </Link>
-        )}
+        <Track link={mangaDetail.mal}>
+          <MALSvg />
+        </Track>
+        <Track link={mangaDetail.aniList}>
+          <AniList />
+        </Track>
+        <Track link={mangaDetail.mangaUpdates}>
+          <MangaUpdate />
+        </Track>
       </Flex>
     </Box>
   );
